e1835ec561d38ea2724385ae1c86680f06ea1f07
dondvdlaan <dondvdlaan@gmail.com>
2024-01-15 14:01:44 +0100

    GitHub test

--- V4/Java-BE/CityWeather/src/main/java/dev/manyroads/weather/location/repository/CityWeatherRepository.java
+++ V4/Java-BE/CityWeather/src/main/java/dev/manyroads/weather/location/repository/CityWeatherRepository.java
@@ -1,6 +1,5 @@
 package dev.manyroads.weather.location.repository;
 
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
